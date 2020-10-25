4a457181c011ad24a79f7fb360c26422731b58e8
Mehedi Hasan <73381645+mehedi150@users.noreply.github.com>
2020-10-25 22:57:19 +0600

    Add files via upload

---
+++ Round function introduction.c
@@ -0,0 +1,15 @@
+//The use of Round function for the first time
+#include <stdio.h>
+#include<math.h>
+int
+main ()
+{
+  double x = 5.35;
+  double result = round (x);
+
+
+  printf ("Round(%lf)", result);
+
+
+  return 0;
+}
