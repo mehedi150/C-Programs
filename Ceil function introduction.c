f5f2c4447dde4e3516ccb8a1dec0d15aa5568dc5
Mehedi Hasan <73381645+mehedi150@users.noreply.github.com>
2020-10-26 22:38:47 +0600

    Add files via upload

---
+++ Ceil function introduction.c
@@ -0,0 +1,13 @@
+//Introduction to ceil function
+#include<stdio.h>
+#include<math.h>
+int
+main ()
+{
+  double x = 2.55;
+  double result = ceil (x);
+  printf ("ceil(%lf) = %lf", x, result);
+
+
+  return 0;
+}
