29461079f115e77ac4a18447bbe35fe116d9b5a9
Mehedi Hasan <73381645+mehedi150@users.noreply.github.com>
2020-10-26 22:36:52 +0600

    Add files via upload

---
+++ Trunc function introduction.c
@@ -0,0 +1,13 @@
+//Introduction to trunc function
+#include<stdio.h>
+#include<math.h>
+int
+main ()
+{
+  double x = 2.55;
+  double result = trunc (x);
+  printf ("trunc(%lf) = %lf", x, result);
+
+
+  return 0;
+}
