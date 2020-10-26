5e4d70dfb765667d845a19e1ee7bc2c37919f613
Mehedi Hasan <73381645+mehedi150@users.noreply.github.com>
2020-10-26 22:40:25 +0600

    Add files via upload

---
+++ Floor function introduction.c
@@ -0,0 +1,13 @@
+//Introduction to floor function
+#include<stdio.h>
+#include<math.h>
+int
+main ()
+{
+  double x = 2.55;
+  double result = floor (x);
+  printf ("floor(%lf) = %lf", x, result);
+
+
+  return 0;
+}
