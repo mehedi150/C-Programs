84cef98e619e6534acc197bca46dbaf803fbf165
Mehedi Hasan <73381645+mehedi150@users.noreply.github.com>
2020-10-27 15:15:34 +0600

    Add files via upload

---
+++ If-else 2.c
@@ -0,0 +1,18 @@
+//Conditional Statement: If-else
+
+#include <stdio.h>
+
+int
+main ()
+{
+   int num;
+   printf("Enter an integer: ");
+   scanf("%d",&num);
+   if (num%2!=0)
+   printf("Odd\n");
+   else 
+   printf("Even\n");
+
+
+  return 0;
+}
---
+++ If-else 3.c
@@ -0,0 +1,22 @@
+//Conditional Statement: If-else
+
+#include <stdio.h>
+
+int
+main ()
+{
+int time=12;
+
+if(time==10)
+{
+    printf("You are on time\n");
+    printf("Kakashi\n");
+}
+else
+{
+    printf("You are late\n");
+    printf("Obito");
+}
+
+  return 0;
+}
