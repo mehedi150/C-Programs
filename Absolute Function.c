9640133e28bf69b1263b31a9180939dbffd95d55
Mehedi Hasan <73381645+mehedi150@users.noreply.github.com>
2021-01-23 15:20:35 +0600

    Add files via upload

---
+++ Absolute Function.c
@@ -0,0 +1,14 @@
+//Absolute Function
+#include<stdio.h>
+int
+main ()
+{
+  int numb, result;
+  printf ("Enter the number: ");
+  scanf ("%d", &numb);
+  result = abs (numb);
+  printf ("The absolute function is: %d", result);
+  return 0;
+
+
+}
