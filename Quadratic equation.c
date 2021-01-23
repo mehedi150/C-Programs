e018d980b7863f30a459bcad001e4a1c85686143
Mehedi Hasan <73381645+mehedi150@users.noreply.github.com>
2021-01-23 15:13:54 +0600

    Update Quadratic equation.c

--- Quadratic equation.c
+++ Quadratic equation.c
@@ -6,8 +6,8 @@ int main()
     printf("Enter a b c values= ");
     scanf("%lf %lf %lf",&a,&b,&c);
     d=sqrt(b*b-4*a*c);
-    x1= (-b+d)/2*a;
-    x2= (-b-d)/2*a;
+    x1= (-b+d)/(2*a);
+    x2= (-b-d)/(2*a);
     printf("Value of x1= %lf\n",x1);
     printf("Value of x2= %lf",x2);
     
