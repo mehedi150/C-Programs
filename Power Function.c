360597cb2bf1ba878b8ab64284a3bff5f4a0608b
Mehedi Hasan <73381645+mehedi150@users.noreply.github.com>
2021-01-23 16:05:40 +0600

    Add files via upload

---
+++ Power Function.c
@@ -0,0 +1,13 @@
+//Power Function
+#include<stdio.h>
+#include<math.h>
+int
+main ()
+{
+  int result;
+  int x = 9;
+  int y = 2;
+  result = pow (x, y);
+  printf ("The power function is: %d", result);
+  return 0;
+}
